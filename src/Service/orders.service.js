107db01d28aa63e007d4b70265d6419bb37dd434
atomic-dzn <wolfb0901@gmail.com>
2025-08-14 22:03:36 +0500

    update

--- src/Service/orders.service.js
+++ src/Service/orders.service.js
@@ -4,21 +4,33 @@ const crypto = require("crypto");
 class order {
     async received_order(data) {
         try {
-            data.order_id = crypto.randomBytes(3).toString('hex');
-            data.total_price = data.quantity * data.price;
+            const order_id = crypto.randomBytes(3).toString('hex');
+            let total_price = 0;
+            for (const item of data.items) {
+                total_price += item.quantity * item.price;
 
-            const sql = `INSERT INTO orders SET ?`;
-            const result = await queryService.dbQuery(sql, [data]);
+                const orderData = {
+                    order_id,
+                    product_id: item.product_id,
+                    quantity: item.quantity,
+                    price: item.price,
+                    total_price: item.quantity * item.price,
+                    user_id: data.user_id 
+                };
 
-            const sql1 = `UPDATE products SET quantity = quantity - ? WHERE product_id = ?`;
-            const values1 = [data.quantity, data.product_id];
-            const result1 = await queryService.dbQuery(sql1, values1);
+                const sql = `INSERT INTO orders SET ?`;
+                const result = await queryService.dbQuery(sql, [orderData]);
 
+                if (!result?.affectedRows) {
+                    throw new Error("Failed to receive order");
+                }
 
-            if (!result?.affectedRows) {
-                throw new Error("Failed to receive order");
+                const sql1 = `UPDATE products SET quantity = quantity - ? WHERE product_id = ?`;
+                const values1 = [item.quantity, item.product_id];
+                await queryService.dbQuery(sql1, values1);
             }
-            return "item(s) have been ordered successfully.";
+
+            return `Order placed successfully. Total price: ${total_price}`;
         } catch (err) {
             throw err;
         }
